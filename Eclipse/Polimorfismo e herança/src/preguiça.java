e77eb61e72fa39a0955ea00e53acae86e89b0fba
Guribas <guu.ribas@icloud.com>
2021-07-15 17:18:49 -0300

    exercicio

---
+++ Eclipse/Polimorfismo e herança/src/preguiça.java
@@ -0,0 +1,16 @@
+pacote  PROGRMAS ;
+
+import  OrientacaoOBEJTO.Animal ;
+
+public  class  Preguica  extends  Animal {
+	// Metodos
+	public  void  subirArvore ()
+	{
+		Sistema . para fora . println ( " Preguica subindo em arvores ... " );
+	}
+	
+	public  void  emitirSom ()
+	{
+		Sistema . para fora . println ( " AAAAAAHHHHZZZZ " );
+	}
+}
