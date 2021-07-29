4d59ead3d0a607aaaeae7c8b3c1f48f40811c3c8
Guribas <guu.ribas@icloud.com>
2021-07-29 08:43:16 -0300

    exercicios

---
+++ turma27java/Eclipse/BlogPessoaal/src/repositorio/postagem.java
@@ -0,0 +1,13 @@
+package  org.generation.blogPessoal.repository ;
+
+import  java.util.List ;
+
+import  org.generation.blogPessoal.model.Postagem ;
+import  org.springframework.data.jpa.repository.JpaRepository ;
+import  org.springframework.stereotype.Repository ;
+
+
+@Repositório
+public  interface  PostagemRepository  extends  JpaRepository < Postagem , Long > {
+	 Lista  pública < Postagem > findAllByTituloContainingIgnoreCase ( String  titulo );
+}
---
+++ turma27java/Eclipse/BlogPessoaal/src/repositorio/tema.java
@@ -0,0 +1,11 @@
+package  org.generation.blogPessoal.repository ;
+
+import  java.util.List ;
+
+import  org.generation.blogPessoal.model.Tema ;
+import  org.springframework.data.jpa.repository.JpaRepository ;
+
+ interface  pública TemaRepository  estende  JpaRepository < Tema , Long > {
+	 Lista  pública < Tema > findAllByDescricaoContainingIgnoreCase ( String  descricao );
+
+}
